175c6b7f2774c096ad288de5da93326f3b9110e3
adaptivebehaviour <110525929+adaptivebehaviour@users.noreply.github.com>
2026-04-14 14:37:33 +1000

    Place the collection instance in the component to ensure it is available for the server component.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,12 @@
 import {collect, JSX, ServerCollectionInstance} from '@/manifesto/collect';
 
-const collection = collect() as ServerCollectionInstance;
 
-let clientFetchComponent = collection.construct({"{$text}": ["{@backend}"]}) as JSX;
-let text = await collection.fetch("{@backend}");
-let serverFetchComponent = collection.construct(text) as JSX;
 
 export default async function Home() {
+    const collection = collect() as ServerCollectionInstance;
+    let clientFetchComponent = collection.construct({"{$text}": ["{@backend}"]}) as JSX;
+    let text = await collection.fetch("{@backend}");
+    let serverFetchComponent = collection.construct(text) as JSX;
     return (<div>{clientFetchComponent}{serverFetchComponent}</div>);
 }
  
